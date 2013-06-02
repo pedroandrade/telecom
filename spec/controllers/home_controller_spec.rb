e74ad85fc1f2c457b4d8361b91d9720e3df3de76
Pedro Andrade <pedro.rjandrade@gmail.com>
2013-06-02 08:52:30 -0300

    remove spec/helpers and add tests for home_controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe HomeController do
+  
+  let(:valid_attributes) { service_valid_attributes }
 
+  let(:service) { create_service }
+  
+  it 'assigns all services as @services in homepage' do
+    get :index, {}
+    assigns(:services).should eq([service])
+  end
+  
 end
