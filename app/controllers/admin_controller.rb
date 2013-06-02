bcdf5a2fec4dc7f3dceabf0c26a6e6685d175f0c
Pedro Andrade <pedro.rjandrade@gmail.com>
2013-06-02 12:47:06 -0300

    fix spaces

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,10 @@ class AdminController < ApplicationController
 
   before_filter :authenticate
 
-	def authenticate
-		authenticate_or_request_with_http_basic do |username, password|
-  		username == "pedro" && password == "123456"
-  	end
-	end
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "pedro" && password == "123456"
+    end
+  end
 
 end
